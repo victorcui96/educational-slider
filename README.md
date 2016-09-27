dd5673f8a99a07ada7141708482ff4e43925d7b9
Victor Cui <victor.cui1996@gmail.com>
2016-09-27 15:53:39 -0400

    add web link to project in README, remove unnecessary comments in SASS

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # mejo586-application-dashboard
 First HW assignment for my MEJO 586 class at UNC, fall 2016. We were given a PSD and told to replicate it exactly, using responsive web design.
+View the project at: http://cuidesign.net/static1/app-dashboard/
---
+++ sass/test.js
@@ -0,0 +1,17 @@
+(function(window, document) {
+	
+})(window, document);
+
+
+var area = (function(window, document) {
+	
+})(window, document);
+
+var hotel = {
+	name: 'Wuay',
+	rooms: 40,
+	booked: 25,
+	checkAvailability: function () {
+		 return this.rooms - this.booked; 
+	}
+};
